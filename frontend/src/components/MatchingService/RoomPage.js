b814550c5f4c0f27e14a6540aa86b6b11b6c9502
dionegoh <77088875+dionegoh@users.noreply.github.com>
2022-11-07 16:18:07 +0800

    [milestone 3] Question service (#160)
    * npm init
    
    * Implement skeleton of question service (No Functionality)
    
    * Implement Question Service - Retrieve Qn by difficulty API (#104)
    
    * Add API to retrieve a question based on difficulty
    
    - Correct Typo "Diffficulty"
    
    * Add randomness to question retrieval
    
    - Additionally, implemented API to generate a new question that is not the same as current question
    
    * [#113] Reflect question on room page
    
    **Summary**
    - sent a `GET` request  the question-service api by using `axios`
    - retrieved `response.data` and passed it to the room page
    
    * (#105) FR3.1 The system contains questions with easy, medium and hard difficulty levels
    
    * Add API to retrieve a question based on difficulty
    
    * Add randomness to question retrieval
    
    * Additionally, implemented API to generate a new question that is not the same as current question
    
    * Add questions to mongodb using json files
    
    * Add and update 20 easy, medium, hard questions
    
    Co-authored-by: fantablack <fantablack@users.noreply.github.com>
    
    * FR3.X UI for displaying questions #110
    
    Summary:
    - Add a separate component for question display
    - Fix new line ("\n") not showing on UI
    - Todo: Add a button component and its related UX flow for generating new questions
    
    * Implement storing and displaying of pictures for questions (#131)
    
    **Summary**
    * Questions currently only consist of string data, which may not be sufficient as some questions require image examples to help the user better understand the question.
    * Update Question Bank and Question Schema to support base64 encoded image data.
    * Implement rendering of image data if it exists in `RoomPage.js` and `QuestionDisplay.js`.
    
    * [#153] Update question bank with more images 
    
    **Summary**
    * Update questions across all easy, medium and hard questions with corresponding image if it exists
    
    * [#142] refresh question
    
    
    ### Implemented functionality to refresh question in the room page 
    - Frontend: added refresh icon and modal check for confirmation 
    - Backend: handled socket.io logic (client emits `refresh-question` to indicate a refresh event and server emits `update-question` with the new question as an argument)
    
    * [#142] refresh question
    
    ### Implemented functionality to refresh question in the room page 
    - Frontend: added refresh icon and modal check for confirmation 
    - Backend: handled socket.io logic (client emits `refresh-question` to indicate a refresh event and server emits `update-question` with the new question as an argument)
    
    * Update UI to align with the rest
    
    Co-authored-by: fantablack <fantablack@users.noreply.github.com>
    Co-authored-by: ureshiiYing <65227564+ureshiiYing@users.noreply.github.com>
    Co-authored-by: Papattarada Apithanangsiri <papattarada1643@gmail.com>

--- frontend/src/components/MatchingService/RoomPage.js
+++ frontend/src/components/MatchingService/RoomPage.js
@@ -18,7 +18,7 @@ import './RoomPage.css';
 
 export default function RoomPage() {
 
-    //styling for leave room modal
+    //styling for modal
     const modal = {
         position: 'fixed',
         left: '0',
@@ -56,8 +56,6 @@ export default function RoomPage() {
     useEffect( () => {
         ensureLoggedIn(navigate);
     });
-
-    console.log(location);
   
     const roomId = location.state  
                    ? location.state.roomId 
@@ -66,39 +64,64 @@ export default function RoomPage() {
     const secondClientSocketId = location.state
                    ? location.state.secondClientSocketId
                    : "";
+    
+    let isFirstQuestion = (window.sessionStorage.getItem("question") == null)
 
-    //breaking question down
-    let questionData = location.state.questionData;
-    let questionDifficulty = questionData.question.QuestionDifficulty;
-    let questionTitle = questionData.question.QuestionTitle;
-    let questionBody = questionData.question.QuestionBody;
-
+    const questionDifficulty = location.state.questionData.question.QuestionDifficulty;    
+    const [questionTitle, setQuestionTitle] = useState(isFirstQuestion 
+                                                        ? location.state.questionData.question.QuestionTitle
+                                                        : JSON.parse(window.sessionStorage.getItem("question")).QuestionTitle);
+    const [questionBody, setQuestionBody] = useState(isFirstQuestion
+                                                     ? location.state.questionData.question.QuestionBody
+                                                     : JSON.parse(window.sessionStorage.getItem("question")).QuestionBody);
+    const [questionImage, setQuestionImage] = useState(isFirstQuestion
+                                                       ? location.state.questionData.question.QuestionImage
+                                                       : JSON.parse(window.sessionStorage.getItem("question")).QuestionImage);
+        
     console.log("roomId" + roomId);
     console.log("socketID " + socket.id);
 
     const [showLeaveModal, setShowLeaveModal] = useState(false);
+    const [showRefreshModal, setShowRefreshModal] = useState(false);
+    
 
     useEffect( () => {
-
         socket.on("connect", () => {
             console.log(socket.connected); // true
           });
         socket.emit("join-room", roomId);
 
+        socket.on("update-question", (question) => {            
+            window.sessionStorage.setItem("question", JSON.stringify(question.question));
+            setQuestionTitle(question.question.QuestionTitle);
+            setQuestionBody(question.question.QuestionBody);
+            setQuestionImage(question.question.QuestionImage);
+            console.log(question.question);
+        });
+
         return () => {
             socket.disconnect();
+            window.sessionStorage.clear();
             socket.connect();
         } 
-
     }, []);
 
-    const handleoOpenModal = (e) => {
+    const handleOpenModal = (e) => {
         setShowLeaveModal(true);
     }
 
     const handleCloseModal = (e) => {
         setShowLeaveModal(false);
     }
+
+    const handleOpenRefreshModal = (e) => {
+        setShowRefreshModal(true);
+    }
+
+    const handleCloseRefreshModal = (e) => {
+        setShowRefreshModal(false);
+    }
+
     
     const onLeaveHandler = (e) => {
         console.log("leaving " + socket.id);
@@ -106,8 +129,10 @@ export default function RoomPage() {
         navigate('/selectquestiondifficulty');
     }
 
-    
-
+    const refreshHandler = (e) => {
+        socket.emit("refresh-question", roomId, questionDifficulty, questionTitle);
+        setShowRefreshModal(false);
+    }
 
     return (
         <>
@@ -129,20 +154,31 @@ export default function RoomPage() {
                 getGutterProps,
             }) => (
                 <div className="grid" {...getGridProps()}>
+                    { showRefreshModal ? 
+                        <div style={modal}>
+                        <div style={center}>
+                            <Typography variant="body1" sx={{padding: '20px'}}> Are you sure you want to change to another question? The change will be reflected to all users in the room. </Typography>
+                            <Box>
+                                <Button variant="contained" onClick={refreshHandler} sx={{margin: '5px', borderRadius: '20px', backgroundColor: '#05CE91', width: "80px"}}> Yes </Button>
+                                <Button variant="contained" onClick={handleCloseRefreshModal} sx={{margin: '5px', borderRadius: '20px', backgroundColor: '#FF3152', width: "80px"}}> Cancel </Button>
+                            </Box>
+                        </div>
+                    </div>
+                    : <></>}
                     <Stack spacing={0.5}>
                         {/* room number and leave room button */}
                         <Box sx={{height: "9.5vh", display:'flex', justifyContent:'flex-start', alignItems:'center'}}>
                             {/* room number  */}
                             <Typography variant="body1" sx={{margin: 2}}> Room ID: {roomId.slice(0,8)}  </Typography> 
                             {/* leave room button */}
-                            <Button variant="contained" endIcon={<LogoutIcon />} size="small" sx={{fontSize: '15px', textTransform: 'none', borderRadius: '25px'}} style={{ backgroundColor:"#FF3152", width:"88px" }} onClick={handleoOpenModal}>
+                            <Button variant="contained" endIcon={<LogoutIcon />} size="small" sx={{fontSize: '15px', textTransform: 'none', borderRadius: '25px'}} style={{ backgroundColor:"#FF3152", width:"88px" }} onClick={handleOpenModal}>
                               Leave  
                             </Button>    
                         </Box>
                         {/* question box */}
                         <Box sx={{height: "50vh", display:'flex', flexDirection: 'column', justifyContent:'flex-start', alignItems:'center', 
                                   border: 1.5, borderColor: 'white', borderRadius: 4, overflow: "scroll"}}>
-                            <QuestionDisplay title={questionTitle} body={questionBody}/> 
+                            <QuestionDisplay title={questionTitle} body={questionBody} image={questionImage} handleOpenRefreshModal={handleOpenRefreshModal} handleCloseRefreshModal={handleCloseRefreshModal}/> 
                         </Box>
                         {/*chat box */}
                         <Box sx={{height: "30vh", display:'flex', flexDirection: 'column'}}>
--- frontend/src/components/QuestionService/QuestionDisplay.js
+++ frontend/src/components/QuestionService/QuestionDisplay.js
@@ -1,10 +1,35 @@
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import AutorenewIcon from '@mui/icons-material/Autorenew';
 
-function QuestionDisplay({title, body}) {
+function QuestionDisplay({title, body, image, handleOpenRefreshModal, handleCloseRefreshModal}) {
+
+const refreshHandler = () => {
+    console.log('refresh-button clicked');
+    handleOpenRefreshModal();
+}
+
     return (
         <Box sx={{whiteSpace: 'pre-line'}}>
-            <Typography variant="h6" sx={{margin: 2}}> {title} </Typography> 
-            <Typography variant="body1" display="block" sx={{margin: 2}}> {body} </Typography> 
+            <Box sx={{ display: 'flex',
+                       flexDirection: 'row',
+                       alignItems: 'center', 
+                       padding: 2, 
+                       gap: 1}}>           
+                <Typography variant="h6"> {title} </Typography> 
+                <IconButton onClick={refreshHandler} sx={{backgroundColor: '#F0F0F0',
+                                                          "&:hover": {
+                                                            backgroundColor: '#D0D0D0'
+                                                          }}}>
+                    <AutorenewIcon></AutorenewIcon>
+                </IconButton>
+            </Box>
+                {image !== "" &&
+                    <Grid2 xs={12} padding={2}>
+                        <img src={image} alt={title} style={{ width: "100%", height: "100%", objectFit: 'contain' }} />
+                    </Grid2>
+                }
+                <Typography variant="body1" display="block" sx={{margin: 2}}> {body} </Typography> 
         </Box>
     )
 }
